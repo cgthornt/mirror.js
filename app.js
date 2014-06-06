dfbf2cb2c6e2ebe1bd6a8fcfdd8936ec6a26a1fb
Christopher Thornton <christopher.thornton@onelogin.com>
2014-06-06 13:47:07 -0500

    Small misc improvements

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var http = require('http'),
-  config = require('./config'),
-       _ = require('underscore'),
      url = require('url'),
       qs = require('querystring');
 
@@ -8,7 +6,6 @@ var server = http.createServer(function(request, response) {
   var body = '';
   var contentType = request.headers['content-type'];
   var query = url.parse(request.url, true).query;
-
 
   var resJson = {
     request: {
@@ -19,7 +16,9 @@ var server = http.createServer(function(request, response) {
     }
   };
 
-  request.on('data', function(chunk) { body += chunk;  });
+  request.on('data', function(chunk) {
+    body += chunk.toString();
+  });
   request.on('end', function() {
     var requestBody = {};
 
@@ -45,11 +44,9 @@ var server = http.createServer(function(request, response) {
 
     response.end(JSON.stringify(resJson, null, 2));
   });
-
-
-
 });
 
+var port = process.env.PORT || 8000;
 
-server.listen(config.port);
-console.log("Server now listening on port " + config.port);
+server.listen(port);
+console.log("Server now listening on port " + port);
--- config.js
+++
@@ -1,7 +0,0 @@
-
-var config = {
-  port: 8000
-};
-
-
-module.exports = config;
